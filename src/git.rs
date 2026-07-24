d180c6925b59fe1d6497eb12f8336fcd54448805
SYOP200 <hello@syop200.com>
2026-07-24 19:11:31 -0400

    Create git.rs

---
+++ src/git.rs
@@ -0,0 +1,23 @@
+use std::process::Command;
+
+pub fn branch() -> Option<String> {
+
+    let output = Command::new("git")
+        .args([
+            "branch",
+            "--show-current"
+        ])
+        .output()
+        .ok()?;
+
+    let branch =
+        String::from_utf8_lossy(&output.stdout)
+        .trim()
+        .to_string();
+
+    if branch.is_empty() {
+        None
+    } else {
+        Some(branch)
+    }
+}
