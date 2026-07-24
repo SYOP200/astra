045e39944716e30d8595413cf0d0b8d9ec3eda93
SYOP200 <hello@syop200.com>
2026-07-24 19:25:03 -0400

    Update history.rs

--- src/history.rs
+++ src/history.rs
@@ -1,24 +1,37 @@
-use rustyline::DefaultEditor;
+use rustyline::Editor;
+use rustyline::history::DefaultHistory;
 
 use dirs::home_dir;
 
-pub fn load(editor: &mut DefaultEditor) {
+use crate::completion::AstraCompleter;
+
+
+pub fn load(
+    editor: &mut Editor<AstraCompleter, DefaultHistory>
+) {
 
     let mut path = home_dir().unwrap();
 
     path.push(".astra_history");
 
+
     if path.exists() {
+
         let _ = editor.load_history(&path);
+
     }
 }
 
 
-pub fn save(editor: &mut DefaultEditor) {
+pub fn save(
+    editor: &mut Editor<AstraCompleter, DefaultHistory>
+) {
 
     let mut path = home_dir().unwrap();
 
     path.push(".astra_history");
 
+
     let _ = editor.save_history(&path);
+
 }
