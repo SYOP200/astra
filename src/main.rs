8b74d5de11f72212c7a221e85c68a2400687e58c
SYOP200 <hello@syop200.com>
2026-07-25 12:47:02 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,20 @@ mod completion;
 mod aliases;
 
 fn main() {
+
+    let args: Vec<String> =
+        std::env::args().collect();
+
+
+    if args.contains(&"--version".to_string()) {
+        println!(
+            "astra {}",
+            env!("CARGO_PKG_VERSION")
+        );
+
+        return;
+    }
+
+
     shell::start();
 }
