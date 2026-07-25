2e80cb44b02ff0377d458f6f5345215b8c547457
SYOP200 <hello@syop200.com>
2026-07-24 21:05:31 -0400

    fix bug

--- src/config.rs
+++ src/config.rs
@@ -3,6 +3,8 @@ use std::fs;
 use dirs::home_dir;
 use serde::Deserialize;
 
+use crate::theme::Theme;
+
 #[derive(Deserialize, Clone)]
 pub struct Config {
     pub theme: Option<String>,
@@ -20,17 +22,33 @@ impl Default for Config {
     }
 }
 
+impl Config {
+
+    pub fn theme(&self) -> Theme {
+        Theme::load(
+            self.theme
+                .as_deref()
+                .unwrap_or("default")
+        )
+    }
+}
+
 pub fn load() -> Config {
+
     let mut path = home_dir().unwrap();
 
     path.push(".astrarc");
+
 
     if !path.exists() {
         return Config::default();
     }
 
-    let content = fs::read_to_string(path)
+
+    let content =
+        fs::read_to_string(path)
         .unwrap_or_default();
+
 
     toml::from_str(&content)
         .unwrap_or_default()
