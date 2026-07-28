3d84737ae42e42493d0033339dff4f4209d7e7f9
SYOP200 <hello@syop200.com>
2026-07-28 11:17:59 -0400

    Update history.rs

--- src/history.rs
+++ src/history.rs
@@ -1,35 +1,47 @@
-use rustyline::Editor;
-use rustyline::history::DefaultHistory;
+use std::path::PathBuf;
 
-use dirs::home_dir;
+use rustyline::{
+    history::DefaultHistory,
+    Editor,
+};
 
-use crate::completion::AstraCompleter;
+use crate::{
+    completion::AstraCompleter,
+    config::Config,
+};
 
-
-pub fn load(
-    editor: &mut Editor<AstraCompleter, DefaultHistory>
-) {
-
-    let mut path = home_dir().unwrap();
-
-    path.push(".astra_history");
-
-
-    if path.exists() {
-        let _ = editor.load_history(&path);
+fn expand_home(path: &str) -> PathBuf {
+    if let Some(rest) = path.strip_prefix("~/") {
+        if let Some(home) = dirs::home_dir() {
+            return home.join(rest);
+        }
     }
+
+    PathBuf::from(path)
 }
 
+pub fn load(
+    readline: &mut Editor<AstraCompleter, DefaultHistory>,
+    config: &Config,
+) {
+    if !config.history.enabled {
+        return;
+    }
+
+    let path = expand_home(&config.history.file);
+
+    let _ = readline.load_history(&path);
+}
 
 pub fn save(
-    editor: &mut Editor<AstraCompleter, DefaultHistory>
+    readline: &mut Editor<AstraCompleter, DefaultHistory>,
+    config: &Config,
 ) {
+    if !config.history.enabled {
+        return;
+    }
 
-    let mut path = home_dir().unwrap();
+    let path = expand_home(&config.history.file);
 
-    path.push(".astra_history");
-
-
-    let _ = editor.save_history(&path);
-
+    let _ = readline.save_history(&path);
 }
