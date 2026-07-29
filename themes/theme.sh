cff73e965adaad5446db2f779f95419707799c58
SYOP200 <hello@syop200.com>
2026-07-29 12:49:13 -0400

    Create theme.sh

---
+++ themes/theme.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Which theme do you want to use?"
+echo ""
+echo "Crimson  : 1"
+echo "Default  : 2"
+echo "Matrix   : 3"
+echo "Midnight : 4"
+echo "Obsidian : 5"
+
+# work in progress...
